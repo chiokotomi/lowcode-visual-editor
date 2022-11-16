9c23edb754dc6ec443dc2420db81802065de6999
chiokotomi <kuroneko.kotomi@gmail.com>
2022-11-16 22:11:54 +0800

    Add 添加自定义搜索组件

---
+++ src/packages/customWidgets/veSearch/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { Search } from 'vant';
+defineProps({
+    background: {
+        type: String,
+    },
+    shape: {
+        type: String,
+    }
+});
+</script>
+
+<template>
+    <div>
+        <Search :background="background" :shape="shape"/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
